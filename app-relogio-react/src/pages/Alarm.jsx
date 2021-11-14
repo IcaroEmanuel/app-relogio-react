810aee416a14d8500724cab6d21120cdfee40151
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-14 18:23:24 -0300

    conteúdo temporário dos componentes

---
+++ app-relogio-react/src/pages/Alarm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Alarm extends Component {
+  render() {
+    return (
+      <section>
+        Alarme
+      </section>
+    );
+  };
+}
---
+++ app-relogio-react/src/pages/Stopwatch.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Stopwatch extends Component {
+  render() {
+    return (
+      <section>
+        Cronômetro
+      </section>
+    );
+  };
+}
