5daf9ab55e16c18b29e4170f7d61382bdc5ccad7
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-14 18:23:39 -0300

    Configurando as rotas

--- app-relogio-react/src/App.jsx
+++ app-relogio-react/src/App.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Watch from './pages/Watch';
+import Alarm from './pages/Alarm'; 
+import Stopwatch from './pages/Stopwatch';
+import Timer from './pages/Timer';
+import PageNotFound from './pages/PageNotFound';
 import './App.css';
 
 class App extends Component{
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Relógio
-        </header>
-      </div>
+      <main className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Watch } />
+            <Route exact path="/alarm" component={ Alarm } />
+            <Route exact path="/stopwatch" component={ Stopwatch } />
+            <Route exact path="/timer" component={ Timer } />
+            <Route path="/*" component={ PageNotFound } />
+          </Switch>
+        </BrowserRouter>
+      </main>
     )
   }
 }
