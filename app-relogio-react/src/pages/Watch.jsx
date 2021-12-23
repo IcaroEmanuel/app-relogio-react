f86d275111dcf50d55de257ba84d1250758a1aa3
icaroemanuel <icaro.emanuel@outlook.com>
2021-12-23 13:29:55 -0300

    Monstrando a hora atual quando a tela é carregada

--- app-relogio-react/src/pages/Watch.jsx
+++ app-relogio-react/src/pages/Watch.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Watch() {
+  const [currentTime, setCurrentTime] = useState('');
+
+  const getTime = () => {
+    const date = new Date;
+    const actualHour = date.toLocaleTimeString();
+    setCurrentTime(actualHour);
+  }
+
+    // useEffect - https://pt-br.reactjs.org/docs/hooks-overview.html
+    useEffect(() => setInterval(() => getTime(), 1000));
+
   return (
     <section>
       Relógio mundial
+      { currentTime }
     </section>
   );
 }
